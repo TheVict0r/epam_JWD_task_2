e847ebea2d24cd82af69fd0e025eb75002b72704
vpravd1@gmail.com <Vv77100369>
2021-04-05 20:35:55 +0300

    Refactoring

--- pravdivets-task2/src/by/epamtc/task2/ex1/exception/NoBallException.java
+++ pravdivets-task2/src/by/epamtc/task2/ex1/exception/NoBallException.java
@@ -3,7 +3,7 @@ package by.epamtc.task2.ex1.exception;
 public class NoBallException extends Exception {
 	
 	public NoBallException() {
-		super ("null value instead of Ball object");
+		super ("Null value instead of Ball object");
 	}
 
 }
--- pravdivets-task2/src/by/epamtc/task2/ex1/exception/NoColorException.java
+++ pravdivets-task2/src/by/epamtc/task2/ex1/exception/NoColorException.java
@@ -2,7 +2,7 @@ package by.epamtc.task2.ex1.exception;
 
 public class NoColorException extends Exception {
 	public NoColorException() {
-		super("null value instead of interface Color object");
+		super("Null value instead of Color object");
 	}
 
 }
